513430228415caba9f90b9335d5510ee821295a5
Tibor Mirnić <tibor.mirnic@leversys.com>
2019-09-18 14:51:21 +0200

    Added more books reducers

--- src/app/shared/state/books.reducer.ts
+++ src/app/shared/state/books.reducer.ts
@@ -1,4 +1,6 @@
+import { createReducer, on, Action } from '@ngrx/store';
 import { BookModel } from "src/app/shared/models/book.model";
+import { BooksPageActions, BooksApiActions } from '../../books/actions';
 
 const createBook = (books: BookModel[], book: BookModel) => [...books, book];
 const updateBook = (books: BookModel[], changes: BookModel) =>
@@ -7,3 +9,58 @@ const updateBook = (books: BookModel[], changes: BookModel) =>
   });
 const deleteBook = (books: BookModel[], bookId: string) =>
   books.filter(book => bookId !== book.id);
+
+export interface State {
+  collection: BookModel[];
+  activeBookId: string | null;
+}
+
+export const initialState: State = {
+  collection: [],
+  activeBookId: null
+};
+
+export const booksReducer = createReducer(
+  initialState,
+  on(
+    BooksPageActions.clearSelectedBook,
+    BooksPageActions.enter,
+    state => {
+      return {
+        ...state,
+        activeBookId: null
+      };
+  }),
+  on(BooksPageActions.selectBook,
+    (state, action) => {
+      return {
+        ...state,
+        activeBookId: action.bookId
+      };
+  }),
+  on(BooksApiActions.bookCreated, (state, action) => {
+    return {
+      ...state,
+      collection: createBook(state.collection, action.book),
+      activeBookId: null
+    };
+  }),
+  on(BooksApiActions.bookUpdated, (state, action) => {
+    return {
+      ...state,
+      collection: updateBook(state.collection, action.book),
+      activeBookId: null
+    };
+  }),
+  on(BooksApiActions.bookDeleted, (state, action) => {
+    return {
+      ...state,
+      collection: deleteBook(state.collection, action.bookId)
+    };
+  })
+);
+
+// Done due to Angular compiler
+export function reducer(state: State | undefined, action: Action) {
+  return booksReducer(state, action);
+}
